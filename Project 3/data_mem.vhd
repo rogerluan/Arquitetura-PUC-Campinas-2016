1ecfb1464b9870e7a7bdcf0592dda164b4f2b468
Roger Oba <rogerluan.oba@gmail.com>
2016-11-02 14:56:31 -0200

    Addedmore generic variables

--- Project 3/data_mem.vhd
+++ Project 3/data_mem.vhd
@@ -2,11 +2,11 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all ;
 
 ENTITY data_mem IS
-	GENERIC ( N : INTEGER := 32 ) ; 
-	PORT ( data_mem_address:  IN STD_LOGIC_VECTOR (N-1 DOWNTO 0);
-		   write_data: IN STD_LOGIC_VECTOR (N-1 DOWNTO 0);
+	GENERIC ( address_size, data_size : INTEGER := 32 ) ; 
+	PORT ( data_mem_address:  IN STD_LOGIC_VECTOR (address_size-1 DOWNTO 0);
+		   write_data: IN STD_LOGIC_VECTOR (data_size-1 DOWNTO 0);
 		   mem_read, mem_write: STD_LOGIC;
-		   read_data: OUT STD_LOGIC_VECTOR (N-1 DOWNTO 0));
+		   read_data: OUT STD_LOGIC_VECTOR (data_size-1 DOWNTO 0));
 END data_mem;
 
 ARCHITECTURE Behavior OF data_mem IS
