e413bc4860cd28afde1a5b3a7fa8c1fdc993723c
Roger Oba <rogerluan.oba@gmail.com>
2016-11-02 13:20:45 -0200

    Design pattern

--- Project 3/alu_component.vhd
+++ Project 3/alu_component.vhd
@@ -5,33 +5,33 @@ USE ieee.std_logic_arith.all;
 USE ieee.std_logic_unsigned.all; 
 
 ENTITY alu_component IS
-	GENERIC ( N : INTEGER := 32 ) ; 
-	PORT ( AluOperand0, AluOperand1:  IN STD_LOGIC_VECTOR (N-1 DOWNTO 0);
-		   AluControl: IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-		   AluZeroFlag: OUT STD_LOGIC;
-		   AluOutput: OUT STD_LOGIC_VECTOR (N-1 DOWNTO 0));
+	GENERIC ( data_size : INTEGER := 32 ) ; 
+	PORT ( alu_operand1, alu_operand2:  IN STD_LOGIC_VECTOR (data_size-1 DOWNTO 0);
+		   alu_control: IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+		   alu_zero_flag: OUT STD_LOGIC;
+		   alu_output: OUT STD_LOGIC_VECTOR (data_size-1 DOWNTO 0));
 		
 END alu_component;
 
 ARCHITECTURE Behavior OF alu_component IS
 BEGIN
-	PROCESS (AluControl)
+	PROCESS (alu_control)
 	BEGIN 
-		CASE AluControl IS
+		CASE alu_control IS
 			WHEN "0010" => --SUM
-				dataOutput <= dataSource0 + dataSource1;
-				AluZeroFlag <= '0';
+				alu_output <= alu_operand1 + alu_operand2;
+				alu_zero_flag <= '0';
 			WHEN "0110" => --SUBTRACTION
-				dataOutput <= dataSource0 - dataSource1;
-				IF dataOutput = '0' THEN
-					AluZeroFlag <= '1';
+				alu_output <= alu_operand1 - alu_operand2;
+				IF alu_output = '0' THEN
+					alu_zero_flag <= '1';
 				END IF ;
 			WHEN "0000" => --AND
-				dataOutput <= dataSource0 AND dataSource1;
-				AluZeroFlag <= '0';
+				alu_output <= alu_operand1 AND alu_operand2;
+				alu_zero_flag <= '0';
 			WHEN "0001" => --OR
-				dataOutput <= dataSource0 OR dataSource1;
-				AluZeroFlag <= '0';
+				alu_output <= alu_operand1 OR alu_operand2;
+				alu_zero_flag <= '0';
 		END CASE;
 	END PROCESS;
 END Behavior;
