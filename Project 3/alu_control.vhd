6e696613d50fc84b733a92874255c2cbc9063077
Roger Oba <rogerluan.oba@gmail.com>
2016-11-02 12:54:43 -0200

    Appled design pattern

--- Project 3/alu_control.vhd
+++ Project 3/alu_control.vhd
@@ -6,31 +6,31 @@ USE ieee.std_logic_unsigned.all;
 
 ENTITY alu_control IS
 	GENERIC ( N : INTEGER := 32 ) ; 
-	PORT ( FUNCT:  IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-		   AluOp: IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-		   AluControl: OUT STD_LOGIC_VECTOR (3 DOWNTO 0));
+	PORT ( funct:  IN STD_LOGIC_VECTOR (5 DOWNTO 0);
+		   alu_op: IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+		   alu_control: OUT STD_LOGIC_VECTOR (3 DOWNTO 0));
 		
 END alu_control;
 
 ARCHITECTURE Behavior OF alu_control IS
 BEGIN
-	PROCESS (AluOp)
+	PROCESS (alu_op)
 	BEGIN 
-		CASE AluOp IS
+		CASE alu_op IS
 			WHEN "00" => --Load/Store
-				AluControl <= "0010"; --SUM
+				alu_control <= "0010"; --SUM
 			WHEN "01" => --branch equal
-				AluControl <= "0110"; --SUBTRACTION
-			WHEN "10" => -- R-Type --The FUNCT field of the instruction determines the ALU operation
-				CASE FUNCT IS 
+				alu_control <= "0110"; --SUBTRACTION
+			WHEN "10" => -- R-Type --The funct field of the instruction determines the ALU operation
+				CASE funct IS 
 					WHEN "100000" => --SUM
-						AluControl <= "0010";
+						alu_control <= "0010";
 					WHEN "100010" => --SUBTRACTION
-						AluControl <= "0110";
+						alu_control <= "0110";
 					WHEN "100100" => --AND
-						AluControl <= "0000";
+						alu_control <= "0000";
 					WHEN "100101" => --OR
-						AluControl <= "0001";
+						alu_control <= "0001";
 				END CASE;
 		END CASE;
 	END PROCESS;
