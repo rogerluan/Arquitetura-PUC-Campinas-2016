121a11cef1260337b1f87a1d98a8b7452dec931d
Roger Oba <rogerluan.oba@gmail.com>
2016-11-02 12:53:52 -0200

    Corrected the order of the instructions to reflect the diagram, so it's easier to understand.

--- Project 2/if_id.vhd
+++ Project 2/if_id.vhd
@@ -3,19 +3,19 @@ USE ieee.std_logic_1164.all ;
 
 ENTITY if_id IS
 	GENERIC ( address_size, instruction_size : INTEGER := 32 ) ; 
-	PORT ( instruction_input: IN STD_LOGIC_VECTOR (instruction_size-1 DOWNTO 0);
-		   next_address_input: IN STD_LOGIC_VECTOR (address_size-1 DOWNTO 0);
+	PORT ( next_address_input: IN STD_LOGIC_VECTOR (address_size-1 DOWNTO 0);
+		   instruction_input: IN STD_LOGIC_VECTOR (instruction_size-1 DOWNTO 0);
 		   clock: IN STD_LOGIC;
-		   instruction_output: OUT STD_LOGIC_VECTOR (instruction_size-1 DOWNTO 0)
-		   next_address_output: OUT STD_LOGIC_VECTOR (address_size-1 DOWNTO 0));
+		   next_address_output: OUT STD_LOGIC_VECTOR (address_size-1 DOWNTO 0)
+		   instruction_output: OUT STD_LOGIC_VECTOR (instruction_size-1 DOWNTO 0));
 END if_id;
 
 ARCHITECTURE Behavior OF if_id IS
 BEGIN
 	PROCESS
 	BEGIN
-	WAIT UNTIL clock'EVENT AND Clock = '1';
-		instruction_output <= instruction_input;
+	WAIT UNTIL clock'EVENT AND clock = '1';
 		next_address_output <= next_address_input;
+		instruction_output <= instruction_input;
 	END PROCESS;
 END Behavior;
